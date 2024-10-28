af6a1f378254ff5dd1558f3ab1a170fe96c14c7e
jota <joaopedroamiguel@gmail.com>
2024-10-28 17:18:52 -0300

    Código inicial do mapa

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import folium
+
+app = Flask(__name__)
+
+@app.route('/')
+def mapa():
+    # Cria o mapa centralizado em uma localização específica
+    mapa = folium.Map(location=[-23.598858, -46.676492], zoom_start=16)
+
+    # Adiciona um marcador de exemplo
+    folium.Marker(
+        location=[-23.598858, -46.676492],
+        popup="Insper EDU",
+        tooltip="Clique para mais informações"
+    ).add_to(mapa)
+
+    # Salva o mapa diretamente na pasta `templates`
+    mapa.save('templates/map.html')
+    return render_template('map.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
